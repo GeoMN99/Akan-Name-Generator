815cf1fa17b3f16ab80f25b6a70d404f2f2189ae
George Ngunjiri <ngunjirigeorge995@gmail.com>
2026-05-01 22:14:31 +0300

    feat(script.js): script user input and add arrays for names

---
+++ script.js
@@ -0,0 +1,13 @@
+// Arrays for Akan names based on day of the week
+var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function getAkanName() {
+    //Get user input
+    var day = parseInt(document.getElementById("day").value);
+    var month = parseInt(document.getElementById("month").value);
+    var month = parseInt(document.getElementById("year").value);
+    var gender = document.getElementById("gender").value;
+
+    
+}
